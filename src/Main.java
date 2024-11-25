a0ee3c3308b7457f938cf9b89ecdf07015b07afb
ElenaSukhomlinova <fandeevaf2@gmail.com>
2024-11-25 19:41:30 +0300

    Homework done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,57 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Задача 1");
+        int [] expenses = {500, 2000, 300, 1500, 800};
+        int sum = 0;
+        int sum1 = 0;
+
+        for (int i = 0; i < expenses.length; i++) {
+            sum += expenses[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+        for (int element : expenses) {
+            sum1 += element;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum1 + " рублей");
+        System.out.println();
+
+        System.out.println("Задача 2");
+        int maxExpenses = -1;
+        int minExpenses = 2500;
+
+        for (int i = 0; i < expenses.length; i++) {
+            if (expenses[i] > maxExpenses) {
+                maxExpenses = expenses[i];
+            }
+        }
+        System.out.println("Максимальная сумма трат за неделю составила " + maxExpenses + " рублей");
+
+        for (int i = 0; i < expenses.length; i++) {
+            if (expenses[i] < minExpenses) {
+                minExpenses = expenses[i];
+            }
+        }
+        System.out.println("Максимальная сумма трат за неделю составила " + minExpenses + " рублей");
+        System.out.println();
+
+        System.out.println("Задача 3");
+        int sum3 = 0;
+        for (int i = 0; i < expenses.length; i++) {
+            sum3 += expenses[i];
+        }
+        float averageExpenses = sum3/4;
+        System.out.println("Средняя сумма трат за месяц составила " + averageExpenses + " рублей");
+        System.out.println();
+
+        System.out.println("Задача 4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+
+        for (int i = 10; i >=0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+
+
     }
 }
